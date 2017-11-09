ac39dca67be13e12a47a5c7a01db2e2a71edcbb4
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-09 16:25:56 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |attribute, hash|
+    hash.each do |sub_attribute, array|
+      array.each do |name|
+        if !(result.keys.include?(name))
+          result[name] = {}
+          result[name][attribute] = []
+        elsif !(result[name].keys.include?(attribute))
+          result[name][attribute] = []
+        end
+          result[name][attribute] << sub_attribute.to_s
+      end
+    end
+  end
+  result
+  # binding.pry
 end
